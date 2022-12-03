ad38c0bd7e57ecaf20e102edeccd5478e09da022
AveRevivus <mambetaliev0606@gmail.com>
2022-12-03 22:00:54 +0600

    Hello

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+
+// const btn = document.querySelector(".btn")
+
+// btn.addEventListener("Click", () => {
+//     alert("HI")
+// })
+
+
+
+// window.addEventListener("load", () => {
+//     alert("Hi")
+// })
+
+
+
+// const btn = document.querySelector(".btn");
+// const btnoff = document.querySelector(".btnoff");
+
+// const text = document.querySelector(".text")
+
+
+// // btn.addEventListener("Click", () => {
+// //     text.style.color = `red`
+// // })
+
+
+
+// btn.addEventListener("click", () => {
+//     text.classList.add("active")
+// })
+
+
+
+// btn.addEventListener("click", () => {
+//     text.classList.add("active")
+// })
+
+
+
+// const select = document.querySelector(".select")
+
+// select.addEventListener("change", (event) => {
+//     const userVariant = event.target.value;
+//     const body = document.body;
+
+//     if(userVariant === "dark") {
+//         body.style.backgroundColor = "black"
+//     } else if (userVariant === "custom") {
+//         const askColor = prompt("Color")
+//         body.style.backgroundColor = askColor
+
+//     } else{
+//         body.style.backgroundColor = "white"
+//     }
+// })
+
+
